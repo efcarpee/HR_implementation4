0a3cf8ee4063a5e447b3c6a063cf1983488981e8
Francois <ecarpeel@trincoll.edu>
2024-10-17 22:01:33 -0400

    Implementación de clases Employee, Hourly, Salaried, HR y corrección de tests

--- src/main/kotlin/edu/trincoll/hr/HR.kt
+++ src/main/kotlin/edu/trincoll/hr/HR.kt
@@ -1,10 +1,19 @@
 package edu.trincoll.hr
 
-// The HR class should have:
-//   - a list of employees
-//   - a hire method that takes an employee and returns a new HR object with that employee added
-//   - a fire method that takes an id and returns a new HR object with the employee with that id removed
-//   - a payEmployees method that returns the total pay of all employees
 class HR(private val employees: List<Employee> = emptyList()) {
 
+    fun hire(employee: Employee): HR {
+        val updatedEmployees = employees.toMutableList()
+        updatedEmployees.add(employee)
+        return HR(updatedEmployees)
+    }
+
+    fun fire(id: Int): HR {
+        val updatedEmployees = employees.filter { it.id != id }
+        return HR(updatedEmployees)
+    }
+
+    fun payEmployees(): Double {
+        return employees.sumOf { it.pay() }
+    }
 }
